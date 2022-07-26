21b04da5a1d9d822edf74189ea928e201ba35954
Arda <ardaras3@gmail.com>
2022-07-26 11:31:31 +0300

    PK correction
    + Timestamp column added as PK

--- create_tables.sql
+++ create_tables.sql
@@ -1,8 +1,9 @@
 CREATE TABLE public.dim_time (
-    year decimal(6, 1) NOT NULL,
+    timestamp decimal(6, 1) NOT NULL,
+    year int4,
     month int4,
     day int4,
     week int4,
     weekday int4,
-    primary key(year)
+    primary key(timestamp)
 );
