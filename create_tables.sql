9c0c6ec85bb85bddb733d5f002c94d136772d789
Arda <ardaras3@gmail.com>
2022-07-28 21:19:16 +0300

    temperatures table added
    + dim_temperatures table added
    + 'if not exists' conditions added

--- create_tables.sql
+++ create_tables.sql
@@ -1,8 +1,15 @@
-CREATE TABLE public.dim_time (
-    timestamp FLOAT NOT NULL,
-    year int4,
-    month int4,
-    day int4,
-    week int4,
-    weekday int4
+CREATE TABLE IF NOT EXISTS public.dim_time (
+    timestamp float NOT NULL,
+    year int,
+    month int,
+    day int,
+    week int,
+    weekday int
+);
+
+CREATE TABLE IF NOT EXISTS public.dim_temperatures (
+    city varchar(256),
+    avg_temp float,
+    avg_temp_uncertainty float,
+    PRIMARY KEY (city)
 );
