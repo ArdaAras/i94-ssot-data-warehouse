f7c5757115c6a6502db58578999484ae201e17c4
Arda <ardaras3@gmail.com>
2022-07-28 16:39:58 +0300

    data type change
    + timestamp is now float

--- create_tables.sql
+++ create_tables.sql
@@ -1,9 +1,8 @@
 CREATE TABLE public.dim_time (
-    timestamp decimal(6, 1) NOT NULL,
+    timestamp FLOAT NOT NULL,
     year int4,
     month int4,
     day int4,
     week int4,
-    weekday int4,
-    primary key(timestamp)
+    weekday int4
 );
